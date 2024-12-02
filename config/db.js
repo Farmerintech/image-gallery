e1f6c2a7b163cc11ce901eb9df010d90b7f09440
unknown <farmerintech@gmail.com>
2024-12-02 12:16:37 +0100

    mongodb atlas connected

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-       await  mongoose.connect(process.env.MONGO_URI);
+       await  mongoose.connect(process.env.MONGO_URI|| "mongodb://127.0.0.1:27017/image-upload-api");
         console.log("MongoDb connected successfully...")
         return
     } catch (error) {
